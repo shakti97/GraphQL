f13a295e7a437625f683a471441eb73ee514b1cb
shakti97 <shaktisingh230797@gmail.com>
2018-11-08 13:46:40 +0530

    added Graphiql tool for testing the query

--- server/app.js
+++ server/app.js
@@ -4,8 +4,10 @@ const app=express();
 const schema=require('./schema/schema.js');
 
 //whenever hit the graphql route it passes it to handle the graphqlHTTP to handle the graphql req
+//graphiql is a in-browser tool for writing , testing queries
 app.use('/graphql',graphqlHTTP({
-
+    schema,
+    graphiql : true
 }));
 
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,39 @@
 const graphql = require('graphql');
+const _=require('lodash');
 
+//Diff between GraphQLID and GraphQLString is String takes always the string where the ID takes number, string etc
 const {
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID
 } = graphql;
 
 
-
+//FakeData
+var books = [{
+        id: '1',
+        name: 'The jungle Book',
+        genre: 'Nature'
+    },
+    {
+        id: '2',
+        name: 'The Harry Potter Book',
+        genre: 'Fantasy'
+    },
+    {
+        id: '3',
+        name: 'Inception',
+        genre: 'Action in Dreams'
+    }
+]
 
 //fields contain the async function which return a object
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: {
-            type: GraphQLString
+            type: GraphQLID
         },
         name: {
             type: GraphQLString
@@ -31,11 +51,12 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: {
                 id: {
-                    type: GraphQLString
+                    type: GraphQLID
                 }
             },
             resolve(parent, args) {
                 //write code to extract the data from the db
+                return _.find(books,{id : args.id});
             }
         }
     }
