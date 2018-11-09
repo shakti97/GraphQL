014c58628613449f4c191850bb6f1ae1fa5d5a41
shakti97 <shaktisingh230797@gmail.com>
2018-11-09 17:35:55 +0530

    connect with mlab

--- server/app.js
+++ server/app.js
@@ -2,6 +2,15 @@ const express=require('express');
 const graphqlHTTP=require('express-graphql');
 const app=express();
 const schema=require('./schema/schema.js');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
+var url= process.env.MONGOLAB_URI;
+var options={useNewUrlParser: true}
+mongoose.connect(url,options);
+mongoose.connection.once('open',()=>{
+    console.log('mlab connected');
+})
 
 //whenever hit the graphql route it passes it to handle the graphqlHTTP to handle the graphql req
 //graphiql is a in-browser tool for writing , testing queries
