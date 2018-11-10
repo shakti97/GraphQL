435c529fea4900226c35ac667e931c5dcc4c764f
shakti97 <shaktisingh230797@gmail.com>
2018-11-10 13:21:59 +0530

    Refactor Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import {BookList} from './Components/BookList';
+import BookList from './Components/BookList';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 
+const client=new ApolloClient({
+  uri : 'http://localhost:8080/graphql'
+})
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
       <div className="App">
         <h1 className='main-heading'>Library</h1>
         <BookList/>
       </div>
+      </ApolloProvider>
     );
   }
 }
